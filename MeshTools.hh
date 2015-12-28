a33cf208f0b51c1cac699e0704a7b4021b4e2c98
demarign <vincent.demarignac@epfl.ch>
2015-12-28 21:35:12 +0100

    multiple vertex merges

---
+++ MeshTools.hh
@@ -0,0 +1,48 @@
+#ifndef MESHTOOLS_HH
+#define MESHTOOLS_HH
+
+#include <ObjectTypes/TriangleMesh/TriangleMesh.hh>
+#include <ObjectTypes/PolyMesh/PolyMesh.hh>
+#include <math.h>
+#include <Eigen/Dense>
+
+class MeshTools
+{
+private:
+   template<typename Vec3Type>
+   PolyMesh::Point vecToP(Vec3Type _v);
+
+   template<typename PType>
+   Eigen::Vector3d pToVec(PType _p);
+
+   template<typename MeshT>
+   Eigen::Vector3d vhToVec(typename MeshT::VertexHandle _h, MeshT* _m);
+
+   Eigen::Vector3d zPlaneRot(double _angle, Eigen::Vector3d _p, Eigen::Vector3d _rot_center);
+
+public:
+   MeshTools() {}
+   ~MeshTools() {}
+
+   void generateQuadRect(PolyMesh* mesh, size_t _width, size_t _height, bool _tessellate = false);
+   void generateTriHex(TriMesh* mesh, size_t _radius, bool _tessellate = false);
+};
+
+/// TEMPLATED FUNCTIONS DEFINITION
+
+template<typename Vec3Type>
+PolyMesh::Point MeshTools::vecToP (Vec3Type _v) { return PolyMesh::Point(_v[0],_v[1],_v[2]); }
+
+template<typename PType>
+Eigen::Vector3d MeshTools::pToVec (PType _p) { return Eigen::Vector3d(_p[0],_p[1],_p[2]); }
+
+template<typename MeshT>
+Eigen::Vector3d MeshTools::vhToVec(typename MeshT::VertexHandle _h, MeshT* _m)
+{ typename MeshT::Point p = _m->point(_h); return pToVec(p); }
+
+#endif // MESHTOOLS_HH
+
+
+
+
+
